ef00e4e11b263c9496ca9381e6265260ad7195de
junsu1023 <55648193+junsu1023@users.noreply.github.com>
2020-01-22 16:34:15 +0900

    SW_Expert_D3

---
+++ SW_Expert_Academy_D3/src/SW_1230.java
@@ -0,0 +1,43 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class SW_1230 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for(int tc=1; tc<=10; tc++){
+            LinkedList<Integer> list = new LinkedList<>();
+            int origin_length = sc.nextInt();
+            for(int i=0; i<origin_length; i++)
+                list.add(sc.nextInt());
+            int order = sc.nextInt();
+            for(int i=0; i<order; i++){
+                String st = sc.next();
+                if(st.equals("I")) {
+                    int where = sc.nextInt();
+                    int many = sc.nextInt();
+                    for (int j = 0; j < many; j++) {
+                        list.add(where, sc.nextInt());
+                        where++;
+                    }
+                }
+                else if(st.equals("D")){
+                    int where = sc.nextInt();
+                    int many = sc.nextInt();
+                    for(int j=0; j<many; j++) {
+                        list.remove(where);
+                    }
+                }
+                else if(st.equals("A")){
+                    int many = sc.nextInt();
+                    for(int j=0; j<many; j++){
+                        list.addLast(sc.nextInt());
+                    }
+                }
+            }
+            System.out.print("#" + tc + " ");
+            for(int i=0; i<10; i++)
+                System.out.print(list.poll() + " ");
+            System.out.println();
+        }
+    }
+}
