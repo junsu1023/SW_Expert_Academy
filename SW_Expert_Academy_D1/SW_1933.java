e38b88196ed4c6b0117a5f806443073d0491cbb3
junsu1023 <55648193+junsu1023@users.noreply.github.com>
2020-02-25 22:22:27 +0900

    SW 1933

---
+++ SW_Expert_Academy_D1/SW_1933.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class SW_1933 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        for(int i=1; i<=num; i++){
+            if(num%i==0) System.out.print(i + " ");
+        }
+    }
+}
