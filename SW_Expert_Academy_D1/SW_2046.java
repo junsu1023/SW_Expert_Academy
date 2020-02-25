5745fef32174d640854d0bcac95fab73bb693066
junsu1023 <55648193+junsu1023@users.noreply.github.com>
2020-02-25 22:25:05 +0900

    SW_2046

---
+++ SW_Expert_Academy_D1/SW_2046.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class SW_2046 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        for(int i=0; i<num; i++)
+            System.out.print("#");
+    }
+}
